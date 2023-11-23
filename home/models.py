d39cf03935351404b9a4af5caf3d71beebb33fef
Breeze Concept <breezeconcept3@gmail.com>
2023-11-23 22:26:08 +0100

    first commit

--- home/models.py
+++ home/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Shortner(models.Model):
-    long_url = models.URLField()
+    long_url = models.URLField(max_length=30000)
     short_code = models.CharField(max_length=15, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
--- urlShortner/settings.py
+++ urlShortner/settings.py
@@ -51,8 +51,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -61,8 +59,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'urlShortner.urls'
 
