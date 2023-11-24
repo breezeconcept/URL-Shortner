2a64252bdfc56c1e4014a53f916f4b56d8f3cb6c
Breeze Concept <breezeconcept3@gmail.com>
2023-11-24 09:51:56 +0100

    modified

--- urlShortner/settings.py
+++ urlShortner/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
 ]
 
 # WhiteNoise configuration for compression and caching
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'urlShortner.urls'
 
@@ -146,8 +146,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 # STATICFILES_DIRS = ('admin', os.path.join(BASE_DIR, 'static', 'admin'))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Change this to your desired directory for collected static files
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Change this to your desired directory for collected static files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
